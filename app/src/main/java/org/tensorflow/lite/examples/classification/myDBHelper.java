23580b3f6d7ba0e1ab72548ba8b17e6c0f168448
busyhuman <busyhuman@naver.com>
2020-05-10 01:22:49 +0900

    Ver1.8

--- app/src/main/java/org/tensorflow/lite/examples/classification/myDBHelper.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/myDBHelper.java
@@ -20,49 +20,28 @@ import java.net.URLDecoder;
 
 public class myDBHelper extends SQLiteOpenHelper {
     Context con;
+    String str;
     public myDBHelper(Context context) {
 
         super(context, "foods", null, 1);
         con = context;
     }
 
+    public void foodUpdate(){
+
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
+
         db.execSQL("DROP TABLE IF EXISTS foods");
-        db.execSQL("CREATE TABLE IF NOT EXISTS foods (Num INTEGER NOT NULL PRIMARY KEY,FoodName VARCHAR(25) NOT NULL,Category VARCHAR(10) NOT NULL, ServingSize FLOAT NOT NULL,Kcal FLOAT NOT NULL,Carbo FLOAT NOT NULL,Protein FLOAT NOT NULL,Fat FLOAT NOT NULL,Natrium FLOAT NOT NULL );");
+
+        db.execSQL("CREATE TABLE foods (Num INTEGER NOT NULL PRIMARY KEY,FoodName VARCHAR(25) NOT NULL,Category VARCHAR(10) NOT NULL, ServingSize FLOAT NOT NULL,Kcal FLOAT NOT NULL,Carbo FLOAT NOT NULL,Protein FLOAT NOT NULL,Fat FLOAT NOT NULL,Natrium FLOAT NOT NULL );");
+
         class NewRunnable implements Runnable {
             @Override
             public void run() {
-
-                String str = SnoopyHttpConnection.makeConnection("https://busyhuman.pythonanywhere.com/foods/?format=json", "GET", null);
-
-
-                try {
-                    JSONArray jarray = new JSONArray(str);
-                    int len = jarray.length();
-                    for(int i=0;i<len;i++){
-                        JSONObject jsonObj = jarray.getJSONObject(i);  // JSONObject 추출
-
-                        System.out.println(jsonObj);
-                        int t_Num = Integer.parseInt(jsonObj.getString("Num"));
-                        String t_FoodName = jsonObj.getString("FoodName");
-                        String t_Category = jsonObj.getString("Category");
-                        float t_ServingSize = Float.parseFloat(jsonObj.getString("ServingSize"));
-                        float t_Kcal = Float.parseFloat(jsonObj.getString("Kcal"));
-                        float t_Carbo = Float.parseFloat(jsonObj.getString("Carbo"));
-                        float t_Protein = Float.parseFloat(jsonObj.getString("Protein"));
-                        float t_Fat = Float.parseFloat(jsonObj.getString("Fat"));
-                        float t_Natrium = Float.parseFloat(jsonObj.getString("Natrium"));
-                        try {
-                            db.execSQL("INSERT INTO foods (Num, FoodName, Category, ServingSize, Kcal, Carbo, Protein, Fat, Natrium) VALUES ('" + t_Num + "', '" + t_FoodName + "', '" + t_Category + "','" + t_ServingSize + "','" + t_Kcal + "','" + t_Carbo + "','" + t_Protein + "','" + t_Fat + "','" + t_Natrium + "');");
-                        } catch (SQLiteException e){
-                            continue;
-                        }
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
+                str = SnoopyHttpConnection.makeConnection("https://busyhuman.pythonanywhere.com/foods/?format=json", "GET", null);
 
             }
         }
@@ -75,6 +54,32 @@ public class myDBHelper extends SQLiteOpenHelper {
             e.printStackTrace();
         }
 
+        try {
+            JSONArray jarray = new JSONArray(str);
+            int len = jarray.length();
+
+            for(int i=0;i<len;i++){
+                JSONObject jsonObj = jarray.getJSONObject(i);  // JSONObject 추출
+
+                System.out.println(jsonObj);
+                int t_Num = Integer.parseInt(jsonObj.getString("Num"));
+                String t_FoodName = jsonObj.getString("FoodName");
+                String t_Category = jsonObj.getString("Category");
+                float t_ServingSize = Float.parseFloat(jsonObj.getString("ServingSize"));
+                float t_Kcal = Float.parseFloat(jsonObj.getString("Kcal"));
+                float t_Carbo = Float.parseFloat(jsonObj.getString("Carbo"));
+                float t_Protein = Float.parseFloat(jsonObj.getString("Protein"));
+                float t_Fat = Float.parseFloat(jsonObj.getString("Fat"));
+                float t_Natrium = Float.parseFloat(jsonObj.getString("Natrium"));
+                try {
+                    db.execSQL("INSERT INTO foods (Num, FoodName, Category, ServingSize, Kcal, Carbo, Protein, Fat, Natrium) VALUES ('" + t_Num + "', '" + t_FoodName + "', '" + t_Category + "','" + t_ServingSize + "','" + t_Kcal + "','" + t_Carbo + "','" + t_Protein + "','" + t_Fat + "','" + t_Natrium + "');");
+                } catch (SQLiteException e){
+                    continue;
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
      //   db.execSQL("INSERT INTO foods ( Num, FoodName, ServingSize, col4 ) SELECT 'col1Val','col2Val','col3Val','col4Val' FROM foods WHERE NOT EXISTS ( SELECT *  FROM `테이블명` WHERE  col1 =  'col1Val' AND  col2 =  'col2Val' AND col3 =  'col3Val');");
     }
     @Override
